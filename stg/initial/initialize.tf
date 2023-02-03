187341b717a5c45450dbcc41e211371e0588e9ca
s.monde@edion.co.jp <s.monde@edion.co.jp>
2023-02-03 14:47:58 +0900

    variables

--- stg/initial/initialize.tf
+++ stg/initial/initialize.tf
@@ -1,7 +1,6 @@
 # Account Alies
 resource "aws_iam_account_alias" "alias" {
-#  account_alias = "${local.resource_prefix}-alias"
-   account_alias = "edion-els-stg-alias"
+  account_alias = "${local.resource_prefix}-alias"
 }
 
 # IAM Group
@@ -20,4 +19,12 @@ resource "aws_iam_account_password_policy" "strict" {
   allow_users_to_change_password = true
 }
 
-
+#Cloudtrail Enable
+resource "aws_cloudtrail" "main" {
+  name                          = "${local.resource_prefix}-trail"
+  s3_bucket_name                = aws_s3_bucket.cloudtrail.id
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  kms_key_id                    = aws_kms_key.cloudtrail.arn
+  s3_key_prefix                 = "cloudtrail"
+}
