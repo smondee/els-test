4930790da9a9e52660702cc1d1b222c5e731de5b
s.monde@edion.co.jp <s.monde@edion.co.jp>
2023-02-06 15:33:18 +0900

    croudtrail + s3

--- stg/initial/initialize.tf
+++ stg/initial/initialize.tf
@@ -127,15 +127,48 @@ resource "aws_iam_role_policy_attachment" "readonly_switch_role" {
 }
 
 #CloudTrail
-resource "aws_cloudtrail" "foobar" {
+resource "aws_cloudtrail" "trail" {
   name                          = "${local.resource_prefix_short}-trail"
-  s3_bucket_name                = aws_s3_bucket.trail.id
+  s3_bucket_name                = aws_s3_bucket.trail_bucket.id
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
 }
 
-resource "aws_s3_bucket" "trail" {
+resource "aws_s3_bucket" "trail_bucket" {
   bucket        = "${local.resource_prefix_short}-traillog"
   force_destroy = true
 }
 
+resource "aws_s3_bucket_policy" "trail_bucket_policy" {
+  bucket = aws_s3_bucket.foo.id
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "${aws_s3_bucket.trail_bucket.arn}"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "${aws_s3_bucket.foo.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
