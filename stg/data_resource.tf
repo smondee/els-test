4c6038fc1577c64ea14aedaa523fbb5affeab03d
s.monde@edion.co.jp <s.monde@edion.co.jp>
2023-02-06 19:12:47 +0900

    data_resource

--- stg/data_resource.tf
+++ stg/data_resource.tf
@@ -53,97 +53,95 @@ data "aws_subnet" "private" {
   }
 }
 
-/* 未構築分を一時コメントアウト
-/* iam */
-data "aws_iam_role" "common" {
-  for_each = {
-    ecs_task_execution = "${local.resource_prefix}-ecs-task-execution"
-    codebuild          = "${local.resource_prefix}-codebuild"
-    codepipeline       = "${local.resource_prefix}-codepipeline"
-
-  }
-  name = each.value
-}
-
-data "aws_iam_policy" "common" {
-  for_each = {
-    ecs_exec   = "${local.resource_prefix}-ecs-exec"
-    common_ecs = "${local.resource_prefix}-common-ecs"
-
-  }
-  arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/${each.value}"
-}
-
-/* RDS */
-data "aws_db_subnet_group" "common" {
-  name = "${local.resource_prefix}-common-db-subnet-group"
-}
-
-/* assumerole */
-data "aws_iam_policy_document" "assumerole" {
-  for_each = toset([
-    "ecs-tasks",
-    "firehose",
-    "ec2",
-    "lambda",
-    "codebuild",
-    "codepipeline",
-    "monitoring.rds",
-    "events",
-    "glue",
-  ])
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["${each.key}.amazonaws.com"]
-    }
-  }
-}
-
-data "aws_s3_bucket" "common" {
-  for_each = {
-    server_log        = "${local.resource_prefix}-server-log"
-    elb_log           = "${local.resource_prefix}-elb-log"
-    ecs_exec_log      = "${local.resource_prefix}-ecs-exec-log"
-    #user_upload_image = "${local.resource_prefix}-user-upload-image"
-    csv               = "${local.resource_prefix}-csv"
-    html              = "${local.resource_prefix}-html"
-    #movie             = "${local.resource_prefix}-movie"
-    config            = "${local.resource_prefix}-config"
-    image             = "${local.resource_prefix}-image"
-    #temporary         = "${local.resource_prefix}-temporary"
-    special           = "${local.resource_prefix}-special"
-    mail              = "${local.resource_prefix}-mail"
-    migration_tools   = "${local.resource_prefix}-migration-tools"
-  }
-  bucket = each.value
-}
-
-/* SG */
-data "aws_security_group" "common" {
-  for_each = {
-    common_sg = "${local.resource_prefix}-common"
-  }
-  filter {
-    name   = "tag:Name"
-    values = [each.value]
-  }
-}
-
-# AMIs
-data "aws_ami" "amazonlinux2_latest" {
-  owners      = ["amazon"]
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0.20220426.0-x86_64-gp2"]
-  }
-}
-
-/* ACM */
-data "aws_acm_certificate" "service_domain_cert_ap_northeast_1" {
-  domain = "*.${local.domains.service}"
-}
-*/
+#/* iam */
+#data "aws_iam_role" "common" {
+#  for_each = {
+#    ecs_task_execution = "${local.resource_prefix}-ecs-task-execution"
+#    codebuild          = "${local.resource_prefix}-codebuild"
+#    codepipeline       = "${local.resource_prefix}-codepipeline"
+#
+#  }
+#  name = each.value
+#}
+#
+#data "aws_iam_policy" "common" {
+#  for_each = {
+#    ecs_exec   = "${local.resource_prefix}-ecs-exec"
+#    common_ecs = "${local.resource_prefix}-common-ecs"
+#
+#  }
+#  arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/${each.value}"
+#}
+#
+#/* RDS */
+#data "aws_db_subnet_group" "common" {
+#  name = "${local.resource_prefix}-common-db-subnet-group"
+#}
+#
+#/* assumerole */
+#data "aws_iam_policy_document" "assumerole" {
+#  for_each = toset([
+#    "ecs-tasks",
+#    "firehose",
+#    "ec2",
+#    "lambda",
+#    "codebuild",
+#    "codepipeline",
+#    "monitoring.rds",
+#    "events",
+#    "glue",
+#  ])
+#  statement {
+#    actions = ["sts:AssumeRole"]
+#
+#    principals {
+#      type        = "Service"
+#      identifiers = ["${each.key}.amazonaws.com"]
+#    }
+#  }
+#}
+#
+#data "aws_s3_bucket" "common" {
+#  for_each = {
+#    server_log        = "${local.resource_prefix}-server-log"
+#    elb_log           = "${local.resource_prefix}-elb-log"
+#    ecs_exec_log      = "${local.resource_prefix}-ecs-exec-log"
+#    #user_upload_image = "${local.resource_prefix}-user-upload-image"
+#    csv               = "${local.resource_prefix}-csv"
+#    html              = "${local.resource_prefix}-html"
+#    #movie             = "${local.resource_prefix}-movie"
+#    config            = "${local.resource_prefix}-config"
+#    image             = "${local.resource_prefix}-image"
+#    #temporary         = "${local.resource_prefix}-temporary"
+#    special           = "${local.resource_prefix}-special"
+#    mail              = "${local.resource_prefix}-mail"
+#    migration_tools   = "${local.resource_prefix}-migration-tools"
+#  }
+#  bucket = each.value
+#}
+#
+#/* SG */
+#data "aws_security_group" "common" {
+#  for_each = {
+#    common_sg = "${local.resource_prefix}-common"
+#  }
+#  filter {
+#    name   = "tag:Name"
+#    values = [each.value]
+#  }
+#}
+#
+## AMIs
+#data "aws_ami" "amazonlinux2_latest" {
+#  owners      = ["amazon"]
+#  most_recent = true
+#  filter {
+#    name   = "name"
+#    values = ["amzn2-ami-hvm-2.0.20220426.0-x86_64-gp2"]
+#  }
+#}
+#
+#/* ACM */
+#data "aws_acm_certificate" "service_domain_cert_ap_northeast_1" {
+#  domain = "*.${local.domains.service}"
+#}
