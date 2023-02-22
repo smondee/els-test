c95a6b2bf212dca93893f68791b91011652ab0bd
smondee <s.monde@edion.co.jp>
2023-02-22 14:17:27 +0900

    rds

--- stg/common/rds.tf
+++ stg/common/rds.tf
@@ -13,12 +13,12 @@ resource "aws_db_subnet_group" "common" {
 
 # monitoring role
 # iam role
-resource "aws_iam_role" "rds_monitoring_role" {
-  name               = "rds_monitoring_role"
-  assume_role_policy = data.aws_iam_policy_document.assumerole["monitoring.rds"].json
-}
+#resource "aws_iam_role" "rds_monitoring_role" {
+#  name               = "rds_monitoring_role"
+#  assume_role_policy = data.aws_iam_policy_document.assumerole["monitoring.rds"].json
+#}
 # iam_role_policy_attachment
-resource "aws_iam_role_policy_attachment" "rds_monitoring" {
-  role       = aws_iam_role.rds_monitoring_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
-}
+#resource "aws_iam_role_policy_attachment" "rds_monitoring" {
+#  role       = aws_iam_role.rds_monitoring_role.name
+#  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+#}
